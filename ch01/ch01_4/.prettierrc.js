4d1a0816000b655ae72b798592b79ab6b0119b77
sjg414 <sjg414@naver.com>
2023-12-21 18:48:35 +0800

    23.12.21 Study_React

---
+++ ch01/ch01_4/.prettierrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  singleQuote: true, //"" -> ''
+  semi: false, //;이 붙은 문장은 모두 삭제
+}
---
+++ ch01/ch01_4/src/index.ts
@@ -0,0 +1,4 @@
+console.log('Hello world!')
+// prettier-ignore
+//프리티어 무시 주석 -> 타입스크립트 컴파일러의 타입체크 기능 무력화(개발이 끝난 코드에만 사용하기 : 컴파일러 정상작동을 위해)
+console.log("Hello");
