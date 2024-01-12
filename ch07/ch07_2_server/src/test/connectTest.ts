2869b2a5523fa29e8a8a7ee136007ebde362195a
sjg414 <sjg414@naver.com>
2024-01-12 22:55:30 +0800

    24.01.12 Study_React

---
+++ ch07/ch07_2_server/src/test/connectTest.ts
@@ -0,0 +1,11 @@
+//DB접속 테스트
+import * as M from '../mongodb'
+
+const connectCB = (db: M.MongoDB) => {
+  console.log('db', db)
+}
+const connectTest = () => {
+  M.connectAndUseDB(connectCB, 'ch07')
+}
+
+connectTest()
---
+++ ch07/ch07_2_server/src/test/insertTest.ts
@@ -0,0 +1,28 @@
+//Create method
+import * as M from '../mongodb'
+
+const connectCB = async (db: M.MongoDB) => {
+  try {
+    const user = db.collection('user')
+    try {
+      await user.drop()
+    } catch (e) {
+      //error ignore
+    }
+
+    const jack = await user.insertOne({name: 'Jack', age: 32})
+    console.log('Jack', jack)
+    const janeAndTom = await user.insertMany([
+      {name: 'Jane', age: 22},
+      {name: 'Tom', age: 11}
+    ])
+    console.log('janeAndTom', janeAndTom)
+  } catch (e) {
+    if (e instanceof Error) console.log(e.message)
+  }
+}
+const insertTest = () => {
+  M.connectAndUseDB(connectCB, 'ch07')
+}
+
+insertTest()
